21492bf54cfeb2175f92aef661b1d3354a0a29b9
AidaDk1 <65682383+AidaDk1@users.noreply.github.com>
2020-11-08 11:21:30 +0600

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -5,12 +5,7 @@ public class Teacher {
     private int salary;
     private int salaryEarned;
 
-    /**
-     * Creates a new Teacher object.
-     * @param id id for the teacher.
-     * @param name name of the teacher.
-     * @param salary salary of the teacher.
-     */
+    
     public Teacher(int id, String name, int salary){
         this.id=id;
         this.name=name;
@@ -18,44 +13,28 @@ public class Teacher {
         this.salaryEarned=0;
     }
 
-    /**
-     *
-     * @return the id of the teacher.
-     */
+   
     public int getId(){
         return id;
     }
 
-    /**
-     *
-     * @return name of the teacher.
-     */
+   
     public String getName(){
         return name;
     }
 
 
-    /**
-     *
-     * @return the salary of the teacher.
-     */
+    
     public int getSalary(){
         return  salary;
     }
 
-    /**
-     * set the salary.
-     * @param salary
-     */
+    
     public void setSalary(int salary){
         this.salary=salary;
     }
 
-    /**
-     * Adds  to salaryEarned.
-     * Removes from the total money earned by the school.
-     * @param salary
-     */
+    
     public void receiveSalary(int salary){
         salaryEarned+=salary;
         School.updateTotalMoneySpent(salary);
